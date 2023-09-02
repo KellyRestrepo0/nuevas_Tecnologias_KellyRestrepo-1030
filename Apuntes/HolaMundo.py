75cfeefff4f5109401f47fefc5823fdb1fa92b5e
KellyRestrepo0 <124794979+KellyRestrepo0@users.noreply.github.com>
2023-09-02 13:47:19 -0500

    Se añadieron archivos
    se agregan archivos de las clases anteriores

---
+++ Apuntes/HolaMundo.py
@@ -0,0 +1,24 @@
+name="Pepito"#STring
+age=25 #entero
+note=3.5 #float
+inMarried= True #boolean
+b=b"Pepito" #byte
+
+print(name)
+print(age)
+#Para conocer el tipo de datos podemos utilizar la funcion typo
+print(type(name))
+print(type(age))
+print(type(note))
+print(type(inMarried))
+print(type(b))
+
+fruits=["Pera","Manzana","Mango"]
+cars=["Mazda","Kia","Chevrolet"]
+constumers={"Nombre":"Pepito","Edad":"19"}
+
+
+print(type(fruits))
+print(type(cars))
+print(type(constumers))
+
